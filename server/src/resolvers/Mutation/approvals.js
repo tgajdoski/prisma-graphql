27d779ef6fe80dd950b80d85331bc08424758f38
tgajdoski <t.gajdoski@gmail.com>
2018-05-15 15:40:12 +0200

    query, update, create, delete - approvals mutation only

--- server/src/resolvers/Mutation/approvals.js
+++ server/src/resolvers/Mutation/approvals.js
@@ -2,48 +2,61 @@ const { getUserId , ApprovalStatus, PublishStatu} = require('../../utils')
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const Lodash = require('lodash');
+const { create_mutation, update_mutation, delete_mutation } = require("../query");
 
  
 const approvalsRef = admin.database().ref('approvals');
 
 const approvals_mutation = {
-  async createApproval(_, { input }, { token }) {
-  if (Lodash.isNil(ctx.request.user)) throw new Error(`Unauthorized request`)
-    return (
-      new Promise((resolve) => {
-        const approval = approvalsRef.push(input, () => {
-          resolve(Object.assign({ id: approval.key }, input)
-          );
-        });
-      })
-    );
+   createApproval(_, {input }, ctx) {
+    return create_mutation({input}, ctx, approvalsRef)
   },
-  async updateApproval(_, { input }, { token }) {
-    if (Lodash.isNil(ctx.request.user)) throw new Error(`Unauthorized request`)
-      const approvalRef = approvalsRef.child(input.id);
-    return approvalRef.once('value')
-      .then(snapshot => {
-        const approval = snapshot.val();
-        if (approval === null) throw new Error('404');
-        return approval;
-      })
-      .then((approval) => {
-        const update = Object.assign(approval, input);
-        delete update.id;
-        return approvalRef.set(update).then(() => (Object.assign({ id: input.id }, update)));
-      });
+  // async createApproval(_, { input }, { token }) {
+  // if (Lodash.isNil(ctx.request.user)) throw new Error(`Unauthorized request`)
+  //   return (
+  //     new Promise((resolve) => {
+  //       const approval = approvalsRef.push(input, () => {
+  //         resolve(Object.assign({ id: approval.key }, input)
+  //         );
+  //       });
+  //     })
+  //   );
+  // },
+
+  updateApproval(_, {input }, ctx) {
+    return update_mutation({input}, ctx, approvalsRef)
   },
-  async deleteApproval(_, { input }, { token }) {
-    if (Lodash.isNil(ctx.request.user)) throw new Error(`Unauthorized request`) 
-    const approvalRef = approvalsRef.child(input.id);
-    return approvalRef.once('value')
-      .then((snapshot) => {
-        const approval = snapshot.val();
-        if (approval === null) throw new Error('404');
-        return Object.assign({ id: input.id }, approval);
-      })
-      .then(approval => approvalRef.remove().then(() => (approval)));
+
+  // async updateApproval(_, { input }, { token }) {
+  //  // if (Lodash.isNil(ctx.request.user)) throw new Error(`Unauthorized request`)
+  //     const approvalRef = approvalsRef.child(input.id);
+  //   return approvalRef.once('value')
+  //     .then(snapshot => {
+  //       const approval = snapshot.val();
+  //       if (approval === null) throw new Error('404');
+  //       return approval;
+  //     })
+  //     .then((approval) => {
+  //       const update = Object.assign(approval, input);
+  //       delete update.id;
+  //       return approvalRef.set(update).then(() => (Object.assign({ id: input.id }, update)));
+  //     });
+  // },
+  async deleteApproval(_, { input }, ctx) {
+    return delete_mutation({input}, ctx, approvalsRef)
   },
+
+  // async deleteApproval(_, { input }, { token }) {
+  //  //  if (Lodash.isNil(ctx.request.user)) throw new Error(`Unauthorized request`) 
+  //   const approvalRef = approvalsRef.child(input.id);
+  //   return approvalRef.once('value')
+  //     .then((snapshot) => {
+  //       const approval = snapshot.val();
+  //       if (approval === null) throw new Error('404');
+  //       return Object.assign({ id: input.id }, approval);
+  //     })
+  //     .then(approval => approvalRef.remove().then(() => (approval)));
+  // },
 }
 
 module.exports = { approvals_mutation }
--- server/src/resolvers/query.js
+++ server/src/resolvers/query.js
@@ -1,12 +1,10 @@
-
 const admin = require("firebase-admin");
 var serviceAccount = require('../qnary-my-fb.json');
 const Lodash = require("lodash");
 
 const query = async ({ id }, ctx, ref) => {
   //  if (Lodash.isNil(ctx.request.user)) throw new Error(`Unauthorized request`)
-  console.log('CCC' , id);
-    if (!Lodash.isNil(id))
+      if (!Lodash.isNil(id))
         ref = ref.child(`/${id}`)
     let value = await admin.database().ref(ref).once("value")
     const res = value.val();
@@ -15,6 +13,48 @@ const query = async ({ id }, ctx, ref) => {
             Object.assign({ id: o }, res[o])
         );
         return Object.assign({ id: id }, res);
-   }
+   };
+  
 
- module.exports = { query }
+   const create_mutation = async ({ input }, ctx, ref) => {
+    //  if (Lodash.isNil(ctx.request.user)) throw new Error(`Unauthorized request`)
+    return (
+        new Promise((resolve) => {
+          const obj = ref.push(input, () => {
+            resolve(Object.assign({ id: obj.key }, input)
+            );
+          });
+        })
+      );
+     };
+
+     const update_mutation = async ({ input }, ctx, ref) => {
+    //  if (Lodash.isNil(ctx.request.user)) throw new Error(`Unauthorized request`)
+        const refPath = ref.child(input.id);
+        return refPath.once('value')
+        .then(snapshot => {
+            const obj = snapshot.val();
+            if (obj === null) throw new Error('404');
+            return obj;
+        })
+        .then((obj) => {
+            const update = Object.assign(obj, input);
+            delete update.id;
+            return refPath.set(update).then(() => (Object.assign({ id: input.id }, update)));
+        });
+    };
+
+    const delete_mutation = async ({ input }, ctx, ref) => {
+        //  if (Lodash.isNil(ctx.request.user)) throw new Error(`Unauthorized request`) 
+         const refPath = ref.child(input.id);
+         return refPath.once('value')
+           .then((snapshot) => {
+             const obj = snapshot.val();
+             if (obj === null) throw new Error('404');
+             return Object.assign({ id: input.id }, obj);
+           })
+           .then(obj => refPath.remove().then(() => (obj)));
+     };
+    
+
+ module.exports = { query , create_mutation , update_mutation, delete_mutation}
